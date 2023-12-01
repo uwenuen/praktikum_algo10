77274d845594a87d61a14412ef8cb6c97eab98f9
uwenuen <145884438+uwenuen@users.noreply.github.com>
2023-12-01 20:58:17 +0700

    pralgo10a.py

--- pralgo10a.py
+++ pralgo10a.py
@@ -1,36 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  1 20:28:39 2023
+Created on Fri Dec  1 20:36:35 2023
 
-@author: Gwen
+@author: user
 """
 
-def binarysearch(arr, start, end, x):
+def binarysea(arr, start, end, n):
     if end >= start:
-        mid = start + (end - start) // 2
-        if arr[mid] == x:
+        mid = start + (end - start)//2
+        if arr[mid] == n:
             return mid
-        elif arr[mid] > x:
-            return binarysearch(arr, start, mid - 1, x)
+        elif arr[mid] > n:
+            return binarysea(arr, start, mid - 1, n)
         else:
-            return binarysearch(arr, mid + 1, end, x)
+            return binarysea(arr, mid + 1, end, n)
     else:
-        return -1
-
-def bubblesort(x):
-    for i in range(len(x)-1):
-        for j in range(0, len(x)-i-1):
-            if x[j] > x[j+1]:
-                x[j], x[j+1] = x[j+1], x[j]
-
-    c = int(input("Masukkan angka yang dicari: "))
-    print("Sesudah di sorting:", x)
-    result = binarysearch(x, 0, len(x)-1, c)
-
-    if result != -1:
-        print(f"terdapat Angka {c} di urutan ke : {result}")
+        return - 1
+def bubblesort(n):
+    for i in range(len(n)-1):
+        for j in range(0, len(n) - i - 1):
+            if n[j] > n[j+1]:
+                n[j], n[j+1] = n[j+1], n[j]
+    nyariangka = int(input("Masukkan angka yang akan dicari: "))
+    print("setelah di sorting: ", n)
+    hasil = binarysea(n, 0, len(n) - 1 , nyariangka)
+    
+    if hasil != -1:
+        print(f"ada Angka {nyariangka} di urutan ke : {hasil}")
     else:
-        print(f"Angka {c} tidak ditemukan dalam list")
+        print(f"Angka {nyariangka} tidak ditemukan dalam list")
 
 array = [87, 54, 34, 23, 89, 15, 2, 200, 28, 31]
 bubblesort(array)
